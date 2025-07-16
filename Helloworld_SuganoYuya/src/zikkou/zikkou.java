f1b5585e38678bfa863e36a107c672bf05de114c
EDO-NET User <EDO-NET User@192.168.0.12>
2025-07-16 20:07:07 +0900

    7/16 課題 1.22

---
+++ Helloworld_SuganoYuya/src/zikkou/zikkou.java
@@ -0,0 +1,10 @@
+package zikkou;
+
+import logic.logic;
+
+public class zikkou {
+    public static void main(String[] args) {
+        logic printer = new logic();
+        printer.printMessages();
+    }
+}
