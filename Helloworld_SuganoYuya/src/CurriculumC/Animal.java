e04533a23a97f38d166bbf4cb9c54426548a289b
EDO-NET User <EDO-NET User@192.168.0.12>
2025-07-16 20:07:48 +0900

    7/16 課題再提出 1.19

--- Helloworld_SuganoYuya/src/CurriculumC/Animal.java
+++ Helloworld_SuganoYuya/src/CurriculumC/Animal.java
@@ -8,11 +8,11 @@ public class Animal {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		Dog dog1 = new Dog();
 		System.out.println(dog1.getAnimal());
-		
+
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
 		Dog dog2 = new Dog(5);
 		System.out.println(dog2.getKazu());
-		
+
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 		LocalDateTime now = LocalDateTime.now();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
--- Helloworld_SuganoYuya/src/CurriculumC/Dog.java
+++ Helloworld_SuganoYuya/src/CurriculumC/Dog.java
@@ -3,21 +3,26 @@ package CurriculumC;
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
 	private String animal;
-	 public String getAnimal() {
-	        return animal;
-	    }
+
+	public String getAnimal() {
+		return animal;
+	}
+
 	// Q2：フィールドに動物の数の変数を定義してください。
-	private int kazu ;
-	 public int getKazu() {
-	        return kazu;
-	    }
+	private int kazu;
+
+	public int getKazu() {
+		return kazu;
+	}
+
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
 	public Dog() {
 		this.animal = "犬";
 	}
+
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	 public Dog(int kazu) {
-	        this.animal = "犬";
-	        this.kazu = kazu;
-	    }
+	public Dog(int kazu) {
+		this.animal = "犬";
+		this.kazu = kazu;
+	}
 }
